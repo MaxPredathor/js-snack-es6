9fcb42d662ebdf1c0bcdd40c3f691f2e59e09646
MaxPredathor <max.gilli98@gmail.com>
2023-10-24 14:17:54 +0200

    snak 2 point 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* ************ SNACK 1 *************** */
 const invitati = [
     'Brad Pitt',
     'Johnny Depp',
@@ -23,3 +24,53 @@ invitati.forEach((Element) =>{
     invitatiFormato.push(pattern);
 });
 console.log(invitatiFormato);
+
+/* ************ SNACK 2 *************** */
+
+const studenti = [
+    {
+        Id: 213,
+        Name: 'Marco della Rovere',
+        Grades: 78
+    },
+    {
+        Id: 110,
+        Name: 'Paola Cortellessa',
+        Grades: 96
+    },
+    {
+        Id: 250,
+        Name: 'Andrea Mantegna',
+        Grades: 48
+    },
+    {
+        Id: 145,
+        Name: 'Gaia Borromini',
+        Grades: 74
+    },
+    {
+        Id: 196,
+        Name: 'Luigi Grimaldello',
+        Grades: 68
+    },
+    {
+        Id: 102,
+        Name: 'Piero della Francesca',
+        Grades: 50
+    },
+    {
+        Id: 120,
+        Name: 'Francesco da Polenta',
+        Grades: 84
+    }       
+];
+
+const aula = studenti.map((Element) =>{
+    return Element.Name.toUpperCase();
+});
+console.log(aula);
+
+
+
+
+
